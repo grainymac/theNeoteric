5f5e6afd284b2acecd9fa7671d2b16e987f5538f
Ian McIntosh <ian.c.mcintosh87@gmail.com>
2023-02-21 19:36:30 -0600

    finished with navbar

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -9,6 +9,13 @@ const NavBar = () => {
       <Link className='logo' to='/'>
         <img src={Logo} alt='logo' />
       </Link>
+      <nav>
+        <NavLink className='nav-link' exact='true' activeclassname='active' to='/'>Home</NavLink>
+        <NavLink className='nav-link' exact='true' activeclassname='active' to='/arts'>Arts</NavLink>
+        <NavLink className='nav-link' exact='true' activeclassname='active' to='/science'>Science</NavLink>
+        <NavLink className='nav-link' exact='true' activeclassname='active' to='/us'>Us</NavLink>
+        <NavLink className='nav-link' exact='true' activeclassname='active' to='/world'>World</NavLink>
+      </nav>
     </div>
   )
 }
