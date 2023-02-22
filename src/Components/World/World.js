2d5c25bbbfff649558db7256955ee9515e761cc9
Ian McIntosh <ian.c.mcintosh87@gmail.com>
2023-02-22 16:50:21 -0600

    added world page and styling

--- src/Components/World/World.js
+++ src/Components/World/World.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import './World.scss'
+import WorldCard from '../WorldCard/WorldCard'
 
-const World = () => {
+const World = ({ world }) => {
+
+  const worldArticles = world.map((article) => {
+    return (
+      <WorldCard title={article.title} abstract={article.abstract} url={article.url} datePublished={article.published_date} author={article.byline} key={article.created_date}/>
+    )
+  return worldArticles
+})
+
   return (
-    <div>World</div>
+    <>
+      <div className='world-page'>
+        <h1 className='world-title'>The Neoteric</h1>
+        <div className='world-article-container'>{worldArticles}</div>
+      </div>
+    </>
   )
 }
 
