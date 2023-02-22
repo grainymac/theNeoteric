b65134ef0267815742b9aaaef6d418479b739b36
Ian McIntosh <ian.c.mcintosh87@gmail.com>
2023-02-22 16:46:10 -0600

    added usCard and styling

--- src/Components/UsCard/UsCard.js
+++ src/Components/UsCard/UsCard.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import './UsCard.scss'
+import { Link } from 'react-router-dom'
 
-const UsCard = () => {
-  return (
-    <div>UsCard</div>
-  )
+const UsCard = ({ title, abstract, url, datePublished, author }) => {
+    return (
+        <>
+            <Link target='_blank' className='us-link' to={url}>
+                <div className='us-card-container'>
+                    <h1 className='us-title'>{title}</h1>
+                    <small className='us-date'>Published: {datePublished}</small>
+                    <small className='us-byline'>{author}</small>
+                    <p className='us-abstract'>{abstract}</p>
+                </div>
+            </Link>
+        </>
+    )
 }
 
 export default UsCard
