cbc98af3b17f36fe17741f923362baadb6b097f9
Ian McIntosh <ian.c.mcintosh87@gmail.com>
2023-02-22 16:43:52 -0600

    added US page

--- src/Components/Us/Us.js
+++ src/Components/Us/Us.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import './Us.scss'
+import UsCard from '../UsCard/UsCard'
 
-const Us = () => {
+const Us = ({ us }) => {
+  
+  const usArticles = us.map((us) => {
+    return (
+      <UsCard title={us.title} abstract={us.abstract} url={us.url} datePublished={us.published_date} author={us.byline} key={us.created_date}/>
+    )
+  return usArticles
+})
+
   return (
-    <div>Us</div>
+    <>
+      <div className='us-page'>
+        <h1 className='us-title'>The Neoteric</h1>
+        <div className='us-article-container'>{usArticles}</div>
+      </div>
+    </>
   )
 }
 
---
+++ src/Components/UsCard/UsCard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UsCard = () => {
+  return (
+    <div>UsCard</div>
+  )
+}
+
+export default UsCard
